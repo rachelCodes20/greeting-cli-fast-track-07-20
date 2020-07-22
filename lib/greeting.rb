61a1927cbf3b141d08b7a811d66116b13e3f2fdb
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-22 01:48:50 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,20 @@
 # code the #greeting method here!
+
+def greeting(name)
+  "Hello #{name}. It's nice to meet you."
+  greeting("HAL")
+  
+end
+
+#def greeting(name)
+  
+  #name = gets.strip
+#puts "I'm HAL, what's your #{name}?"
+
+#greeting("HAL")
+
+#end
+
+
+ 
+
