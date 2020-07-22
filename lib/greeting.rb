6b6903543cdf61270175edef885ff7a16b2a4ad1
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-22 02:04:57 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,15 +2,14 @@
 
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
+  name = gets.strip
+  puts "Hi! I'm HAL, what's your name?"
 end
-greeting("Don")
+greeting()
 
  
 
-def greeting(name)
+
   
-  name = gets.strip
-  puts "Hi! I'm #{HAL}, what's your name?"
-end
-  greeting(name)
+  
   
